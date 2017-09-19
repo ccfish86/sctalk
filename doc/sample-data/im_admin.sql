8b0d316d54bb8661b93383c4f025920d0b94e8da
ccfish86 <ccfish@ccfish.net>
2017-09-19 09:42:36 +0800

    追加测试用数据

---
+++ doc/sample-data/im_admin.sql
@@ -0,0 +1,2 @@
+﻿-- password: admin123
+INSERT INTO `im_admin` (`id`,`uname`,`pwd`,`status`,`created`,`updated`) VALUES (1,'admin','21232f297a57a5a743894a0e4a801fc3',0,0,0);
---
+++ doc/sample-data/im_depart.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO `im_depart` (`id`,`depart_name`,`priority`,`parent_id`,`status`,`created`,`updated`) VALUES (1,'测试部门',123456,0,0,1493984252,1493984252);
