989fd3e62fc692d42373a7c99577e193ea6a4dd4
ccfish86 <ccfish@ccfish.net>
2017-09-27 14:16:45 +0800

    修复包名

--- talk-db-proxy/src/main/java/com/blt/talk/service/aspect/LogAspect.java
+++ talk-db-proxy/src/main/java/com/blt/talk/service/aspect/LogAspect.java
@@ -29,7 +29,7 @@ public class LogAspect {
      *
      * ~ 第一个 * 代表任意修饰符及任意返回值. ~ 第二个 * 定义在web包或者子包 ~ 第三个 * 任意方法 ~ .. 匹配任意数量的参数.
      */
-    @Pointcut("execution(* com.blt.talk.service.remote.impl.*.*(..))")
+    @Pointcut("execution(* com.blt.talk.service.remote.rest.*.*(..))")
     public void logPointcut() {}
 
     @Around("logPointcut()")
