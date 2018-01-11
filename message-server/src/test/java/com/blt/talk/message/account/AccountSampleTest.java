f444c42ef9b6a68af2f4667475935d0cf8721173
ccfish86 <ccfish@ccfish.net>
2018-01-11 13:56:11 +0800

    修复性别错误

--- message-server/src/test/java/com/blt/talk/message/account/AccountSampleTest.java
+++ message-server/src/test/java/com/blt/talk/message/account/AccountSampleTest.java
@@ -25,13 +25,14 @@ public class AccountSampleTest {
         RestTemplate rt = new RestTemplate();
         
         for (int i = 10; i< 100;i++) {
+            System.out.println(i%2);
             RegistReq req = new RegistReq();
             req.setName("" + i);
-            req.setSex(i/2);
+            req.setSex(i%2);
             req.setPhone("0000" + i);
             req.setPassword(SecurityUtils.getInstance().EncryptPass("" + i));
             req.setEmail(i + "@hifipi.com");
-            BaseModel res = rt.postForObject("http://tt.****.com/regist", req, BaseModel.class);
+            BaseModel res = rt.postForObject("http://tt.hifipi.com/regist", req, BaseModel.class);
             System.out.println(res.getCode());
         }
         
