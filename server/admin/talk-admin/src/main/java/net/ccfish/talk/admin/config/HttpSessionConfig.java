64a93c821d9eeb50e139d2c273e8b40e378db4e3
ccfish86 <ccfish@ccfish.net>
2018-01-29 11:07:50 +0800

    暂时搁置后台开发

--- server/admin/talk-admin/src/main/java/net/ccfish/talk/admin/config/HttpSessionConfig.java
+++
@@ -1,8 +0,0 @@
-package net.ccfish.talk.admin.config;
-
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
-
-@EnableHazelcastHttpSession
-public class HttpSessionConfig {
-    
-}
